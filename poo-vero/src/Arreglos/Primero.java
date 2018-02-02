66991e78baf6d903d424caea6a265fd4915a0998
T-102 <T-102@PC245>
2018-02-01 19:20:34 -0600

    Arreglos

---
+++ poo-vero/src/Arreglos/Primero.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arreglos;
+
+/**
+ *
+ * @author T-102
+ */
+public class Primero {
+    public static void main(String[] args) {
+        //Arreglos no mutables
+        int x[]={5,-2,6};
+        //para obtener un elemento del arreglo usamos
+        System.out.println(x[0]);
+        //todos los arreglos no mutables tienen una propiedad para saber su tamaño
+        System.out.println(x.length);
+        //Vamos a iterar el arreglo con un ciclo for tradicional
+        for(int i=0;i<x.length;i++){
+            System.out.println(x[i]);
+        }
+        //Lo mismo pero con el ciclo for mejorado
+        for(int a:x){
+            System.out.println(a);
+        }
+    }
+}
