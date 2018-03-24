fe4aabd39e5e5ca39dab590d822f27116aa2910b
Veronica <Veronica@DESKTOP-2860LOI>
2018-03-24 12:18:57 -0600

    Serializar Prueba 3

--- poo-vero/src/Serializacion/Serializar.java
+++ poo-vero/src/Serializacion/Serializar.java
@@ -7,7 +7,7 @@ import java.io.*;
 public class Serializar {
     public static void main(String[] args) {
         //Primero generamos un usuario
-        Arraylist<Empleado> emp=new Arraylist<>();
+        Usuario u=new Usuario(19,"pedro");
         //Generamos un modelo de la clase que serializaba 
         PersistenciaUsuario p=new PersistenciaUsuario();
         //Ajustamos su atribuot usuario
